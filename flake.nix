3cf13d0ca8a0d6a041d05cc0745a5b698638b0ac
eureka-cpu <github.eureka@gmail.com>
2026-01-16 11:05:18 -0800

    Use dunix to parse dune-project

--- flake.nix
+++ flake.nix
@@ -1,39 +1,42 @@
 {
   description = "Literally the simplest script in existence for changing directories and entering a nix shell.";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs, ... }:
+  inputs.dunix.url = "github:eureka-cpu/dunix/master";
+  outputs = { self, nixpkgs, dunix }:
     let
       inherit (builtins) attrValues;
       inherit (nixpkgs.lib) genAttrs;
       inherit (nixpkgs.lib.systems) flakeExposed;
-      eachSystem = f: genAttrs flakeExposed (system: f (import nixpkgs { inherit system; }));
+      eachSystem = f: genAttrs flakeExposed (system: f (import nixpkgs { inherit system; overlays = [ dunix.overlays.default ]; }));
     in
     {
       packages = eachSystem (pkgs:
         let
+          inherit (builtins) elem elemAt fetchurl attrNames foldl' listToAttrs;
           inherit (pkgs.ocamlPackages) buildDunePackage;
-          inherit (pkgs.lib) cleanSource;
+          inherit (pkgs.lib) cleanSource splitStringBy getLicenseFromSpdxId;
           cmdliner = pkgs.ocamlPackages.cmdliner.overrideAttrs (old:
             let
               version = "2.1.0";
             in
             {
               inherit version;
-              src = builtins.fetchurl {
+              src = fetchurl {
                 url = "https://erratique.ch/software/${old.pname}/releases/${old.pname}-${version}.tbz";
                 sha256 = "sha256:1s9lhkzrblaf1rk0b9lg95622p0jv4qmmby8xg8jzma3rlacc548";
               };
             });
+          dune-project = pkgs.importDuneProject ./dune-project;
+          depends = removeAttrs (listToAttrs (map (dep: { name = dep; value = pkgs.ocamlPackages.${dep}; }) (attrNames (foldl' (acc: dep: acc // dep) { } dune-project.package.depends)))) [ "ocaml" "cmdliner" ];
         in
         {
           default = buildDunePackage {
-            pname = "ns";
-            version = "0.3.1";
+            inherit (dune-project) version;
+            pname = dune-project.package.name;
             src = cleanSource ./.;
-            buildInputs = attrValues {
-              inherit (pkgs.ocamlPackages) bos;
+            buildInputs = attrValues (depends // {
               inherit cmdliner;
-            };
+            });
             doCheck = true;
             nativeCheckInputs = attrValues {
               inherit (pkgs) ocamlformat;
@@ -45,14 +48,25 @@
               ${cmdliner}/bin/cmdliner install tool-support $out/bin/ns $out
               ${cmdliner}/bin/cmdliner install generic-completion $out/share
             '';
-            meta.license = pkgs.lib.licenses.mit;
+            meta =
+              let
+                inherit (dune-project.source) type owner repo;
+                maintainers = map (m: pkgs.lib.maintainers.${(elemAt (splitStringBy (_: curr: elem curr [ " " ]) false m) 0)}) dune-project.maintainers;
+              in
+              {
+                inherit maintainers;
+                description = dune-project.package.synopsis;
+                longDescription = dune-project.package.description;
+                homepage = "https://${type}.com/${owner}/${repo}";
+                license = getLicenseFromSpdxId dune-project.license;
+              };
           };
         });
       devShells = eachSystem (pkgs: {
         default = pkgs.mkShell {
           inputsFrom = [ self.packages.${pkgs.stdenv.hostPlatform.system}.default ];
           packages = attrValues {
-            inherit (pkgs) ocamlformat;
+            inherit (pkgs) ocamlformat nil;
             inherit (pkgs.ocamlPackages) ocaml-lsp odoc;
           };
           shellHook = ''
