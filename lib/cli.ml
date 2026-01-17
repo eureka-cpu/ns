dca947d6500378691f38ae5ce868583594d0c207
[eureka@nixos] <57543709+eureka-cpu@users.noreply.github.com>
2026-01-16 22:31:13 -0800

    Use dunix to parse dune-project (#8)
    Uses `dune-build-info` to automatically get the version of the package. Uses `dunix` to parse the `dune-project` file and wire up all of the package information into `buildDunePackage`.

--- lib/cli.ml
+++ lib/cli.ml
@@ -1,5 +1,6 @@
 module Cli = struct
   open Cmdliner
+  open Error
 
   let target_dir =
     let doc = "Target directory with optional attribute" in
@@ -9,7 +10,11 @@ module Cli = struct
   let cmd entrypoint =
     let doc = "Enter a nix development shell in the target directory" in
     let info =
-      let version = "0.3.1" in
+      let version =
+        match Build_info.V1.version () with
+        | None -> Error.handle_ns_error "missing version information"
+        | Some version -> Build_info.V1.Version.to_string version
+      in
       Cmd.info "ns" ~doc ~version
     in
     Cmd.v info Term.(const entrypoint $ target_dir)
